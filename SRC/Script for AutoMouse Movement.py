a52955450156e845ac1b14fe77d8852a48bd8391
dpatel <darshannbgn@gmail.com>
2022-12-14 23:24:45 -0500

    Create Script for AutoMouse Movement.py

---
+++ SRC/Script for AutoMouse Movement.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+import sys
+from datetime import datetime
+pyautogui.FAILSAFE = False
+numMin = None
+if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
+    numMin = 3 ### set the number of minutes to move mouse
+else:
+    numMin = int(sys.argv[1])
+while(True):
+    x=0
+    while(x<numMin):
+        time.sleep(60)
+        x+=1
+    for i in range(0,200):
+        pyautogui.moveTo(0,i*4)
+    pyautogui.moveTo(1,1)
+    for i in range(0,3):
+        pyautogui.press("shift") ### button press for what you want to use to start
+    print("Movement made at {}".format(datetime.now().time()))
